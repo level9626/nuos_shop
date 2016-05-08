d3db0e9aa7312560889bb2007f2bfe856d076e2b
Sergiy Poustovit <levellevel4@gmail.com>
2016-05-08 22:53:15 +0300

    binding between products & carts throught line item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -9,5 +13,15 @@ class Product < ActiveRecord::Base
 
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'line items are exist')
+      return false
+    end
   end
 end
